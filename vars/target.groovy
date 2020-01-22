da67f391ca7658c67002e7042e97c78b9cc8a3d0
Matius Krisna Gunarno <54306640+krisnagunarno@users.noreply.github.com>
2020-01-22 14:36:41 +0700

    Update target.groovy

--- vars/target.groovy
+++ vars/target.groovy
@@ -1,29 +1,29 @@
 #!/usr/bin/env groovy
-
-def call(Map param){
-        pipeline {
-            agent any
-            stages {
-                stage('Build') {
-                    steps {
-                        sh 'mvn -B -DskipTests clean package'
-                    }
-                }
-                stage('Test') {
-                    steps {
-                        sh 'mvn test'
-                    }
-                    post {
-                        always {
-                            junit 'target/surefire-reports/*.xml'
-                        }
-                    }
-                }
-                stage('Deliver') {
-                    steps {
-                        sh 'sh jenkins/scripts/deliver.sh'
-                    }
-                }
+def remote = [:]
+    remote.name = 'target'
+    remote.host = '172.20.10.4'
+    remote.user = 'root'
+    remote.password = 'future4.0'
+    remote.allowAnyHosts = true
+pipeline {
+    agent any
+    stages {
+        stage('Build') {
+            steps {
+                sshCommand remote: remote, command: "cd /home/futureprogram"
+                sshCommand remote: remote, command: "git clone https://github.com/ericsonrumuy7/Jenkins-future.git"
             }
         }
+        stage('Test') {
+            steps {
+                sshCommand remote: remote, command: "mvn test"
+            }
+        }
+        stage('Deliver') {
+            steps {
+                sshCommand remote: remote, command: "sh jenkins/scripts/deliver.sh"
+            }
+        }
+    }
 }
+
