ec4045dfbadba5c2277b257d486aecc978c2aaec
Matius Krisna Gunarno <54306640+krisnagunarno@users.noreply.github.com>
2020-01-22 18:51:39 +0700

    Update deliver.sh

--- resources/deliver.sh
+++ resources/deliver.sh
@@ -23,5 +23,5 @@ set +x
 echo 'The following command runs and outputs the execution of your Java'
 echo 'application (which Jenkins built using Maven) to the Jenkins UI.'
 set -x
-scp target/${NAME}-${VERSION}.jar root@$1:/home/futureprogram
-ssh root@$1 "java -jar /home/futureprogram/${NAME}-${VERSION}.jar"
+scp target/${NAME}-${VERSION}.jar root@$IPADD:/home/futureprogram
+ssh root@$IPADD "java -jar /home/futureprogram/${NAME}-${VERSION}.jar"
