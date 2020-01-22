ff05ed4b48cf10d33f56998fb2fc1a7d095d4898
Matius Krisna Gunarno <54306640+krisnagunarno@users.noreply.github.com>
2020-01-22 16:33:51 +0700

    Update pipelinetest.groovy

--- vars/pipelinetest.groovy
+++ vars/pipelinetest.groovy
@@ -21,7 +21,7 @@ def call(Map param){
                 }
                 stage('Deliver') {
                     steps {
-                        sh 'sh jenkins/scripts/deliver.sh '+param.ip
+                        sh "sh jenkins/scripts/deliver.sh $param.ip"
                     }
                 }
             }
